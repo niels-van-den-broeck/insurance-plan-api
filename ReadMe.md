45c7efe87f1639987abee68e12d96662a3d6c2d3
Niels Van den Broeck <niels.van.den.broeck@pelckmansuitgevers.be>
2020-12-18 19:01:24 +0100

    feat(authentication): added setup for authentication
    added mongo, mongoose, express-session, passportjs, passport-local and connect-mongo

--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,4 @@ Backend to the insurance plan app.
 
 ## .env 
 
-Please copy over the .env.example file to fill in the necessary env vars. In an actual project, the example file would contain dummy values as to not to expose the sensitive information which is stored on the environment.
+Please copy over the .env.example file to fill in the necessary env vars. In an actual production project, the example file would contain dummy values as to not to expose the sensitive information which is stored on the environment.
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,33 @@
+import mongoose from 'mongoose';
+import session from 'express-session';
+import ConnectMongo from 'connect-mongo';
+
 import app from './app';
 
-const PORT = process.env.PORT || 8080;
+const MongoStore = ConnectMongo(session);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/qover-insurance';
+const SESSION_SECRET = process.env.SESSION_SECRET as string; // Let it crash if no secret.
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.error('Connection to the database failed');
+  process.exit(0);
 });
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.use(session({
+    secret: SESSION_SECRET,
+    store: new MongoStore({ mongooseConnection: db }),
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+});
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
